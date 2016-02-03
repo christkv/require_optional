8eac964c8e31166a8fb483d0d56025b185cee03f
Christian Kvalheim <christkv@gmail.com>
2016-02-03 13:51:54 +0100

    Added initial package.json

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var path = require('path'),
   fs = require('fs'),
   f = require('util').format,
+  resolveFrom = require('resolve-from'),
   semver = require('semver');
 
 var exists = fs.existsSync || path.existsSync;
@@ -25,8 +26,15 @@ var require_optional = function(name, options) {
   options = options || {};
   options.strict = typeof options.strict == 'boolean' ? options.strict : true;
 
+  // Current location
+  var location = __dirname;
+  // Check if we have a parent
+  if(module.parent) {
+    location = module.parent.filename;
+  }
+
   // Locate this module's package.json file
-  var location = find_package_json(__dirname);
+  var location = find_package_json(location);
   if(!location) {
     throw new Error('package.json can not be located');
   }
@@ -43,37 +51,49 @@ var require_optional = function(name, options) {
     throw new Error(f('no optional dependency [%s] defined in peerOptionalDependencies in package.json', parts[0]));
   }
 
+  // Unpack the expected version
   var expectedVersions = object.peerOptionalDependencies[parts[0]];
+  // The resolved package
+  var moduleEntry = undefined;
+  // Module file
+  var moduleEntryFile = name;
 
   try {
     // Validate if it's possible to read the module
-    var moduleEntry = require(name);
-
-    // Resolve the location of the module's package.json file
-    var location = find_package_json(require.resolve(name));
-    if(!location) {
-      throw new Error('package.json can not be located');
-    }
-
-    // Read the module file
-    var dependentOnModule = JSON.parse(fs.readFileSync(f('%s/package.json', location)));
-    // Get the version
-    var version = dependentOnModule.version;
-    // Validate if the found module satisfies the version id
-    if(semver.satisfies(version, expectedVersions) == false
-      && options.strict) {
-        var error = new Error(f('optional dependency [%s] found but version [%s] did not satisfy constraint [%s]', parts[0], version, expectedVersions));
-        error.code = 'OPTIONAL_MODULE_NOT_FOUND';
-        throw error;
-    }
-
-    // Satifies the module requirement
-    return moduleEntry;
+    moduleEntry = require(moduleEntryFile);
   } catch(err) {
-    if(err.code != 'MODULE_NOT_FOUND') {
-      throw err;
+    // Attempt to resolve in top level package
+    try {
+      // Get the module entry file
+      moduleEntryFile = resolveFrom(process.cwd(), name);
+      if(moduleEntryFile == null) return undefined;
+      // Attempt to resolve the module
+      moduleEntry = require(moduleEntryFile);
+    } catch(err) {
+      if(err.code === 'MODULE_NOT_FOUND') return undefined;
     }
   }
+
+  // Resolve the location of the module's package.json file
+  var location = find_package_json(require.resolve(moduleEntryFile));
+  if(!location) {
+    throw new Error('package.json can not be located');
+  }
+
+  // Read the module file
+  var dependentOnModule = JSON.parse(fs.readFileSync(f('%s/package.json', location)));
+  // Get the version
+  var version = dependentOnModule.version;
+  // Validate if the found module satisfies the version id
+  if(semver.satisfies(version, expectedVersions) == false
+    && options.strict) {
+      var error = new Error(f('optional dependency [%s] found but version [%s] did not satisfy constraint [%s]', parts[0], version, expectedVersions));
+      error.code = 'OPTIONAL_MODULE_NOT_FOUND';
+      throw error;
+  }
+
+  // Satifies the module requirement
+  return moduleEntry;
 }
 
 require_optional.exists = function(name) {
